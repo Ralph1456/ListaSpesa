1dba1bed7a6fd21c9ed65e40da5acc76d41ae9bf
Ralph1456 <irvine.kentsouing@edu.unifi.it>
2025-06-26 05:13:34 +0200

    nuova versione

--- tests/test_ShoppingList.cpp
+++ tests/test_ShoppingList.cpp
@@ -1,134 +1,239 @@
 #include <gtest/gtest.h>
-#include "ShoppingList.h"
 #include "Item.h"
 #include "Category.h"
-#include <memory>
+#include "ShoppingList.h"
 
+
+class ShoppingListItemTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        category = std::make_shared<FoodCategory>("Frutta e Verdura", "Frutta");
+        item = std::make_unique<Item>("Mele", 3, category, "Bio");
+    }
+
+    std::shared_ptr<Category> category;
+    std::unique_ptr<Item> item;
+};
+
+TEST_F(ShoppingListItemTest, Constructor) {
+    EXPECT_EQ(item->getName(), "Mele");
+    EXPECT_EQ(item->getQuantity(), 3);
+    EXPECT_FALSE(item->isPurchased());
+    EXPECT_EQ(item->getCategory(), category);
+    EXPECT_EQ(item->getNotes(), "Bio");
+    EXPECT_GT(item->getId(), 0);
+}
+
+TEST_F(ShoppingListItemTest, DefaultConstructor) {
+    Item defaultItem("Pane");
+    EXPECT_EQ(defaultItem.getName(), "Pane");
+    EXPECT_EQ(defaultItem.getQuantity(), 1);
+    EXPECT_FALSE(defaultItem.isPurchased());
+    EXPECT_EQ(defaultItem.getCategory(), nullptr);
+    EXPECT_EQ(defaultItem.getNotes(), "");
+}
+
+TEST_F(ShoppingListItemTest, SettersAndGetters) {
+    item->setName("Pere");
+    item->setQuantity(5);
+    item->setPurchased(true);
+    item->setNotes("Mature");
+
+    EXPECT_EQ(item->getName(), "Pere");
+    EXPECT_EQ(item->getQuantity(), 5);
+    EXPECT_TRUE(item->isPurchased());
+    EXPECT_EQ(item->getNotes(), "Mature");
+}
+
+TEST_F(ShoppingListItemTest, InvalidQuantity) {
+    int originalQuantity = item->getQuantity();
+    item->setQuantity(-1);
+    EXPECT_EQ(item->getQuantity(), originalQuantity);
+
+    item->setQuantity(0);
+    EXPECT_EQ(item->getQuantity(), originalQuantity);
+}
+
+TEST_F(ShoppingListItemTest, UniqueIds) {
+    Item item1("Item1");
+    Item item2("Item2");
+    EXPECT_NE(item1.getId(), item2.getId());
+}
+
+TEST_F(ShoppingListItemTest, EqualityOperators) {
+    Item item2("Diverse", 1, nullptr, "");
+    EXPECT_NE(*item, item2);
+    EXPECT_TRUE(*item == *item);
+}
+
+TEST_F(ShoppingListItemTest, CategoryAssignment) {
+    auto newCategory = std::make_shared<HouseholdCategory>("Latticini", "Prodotti caseari");
+    item->setCategory(newCategory);
+    EXPECT_EQ(item->getCategory(), newCategory);
+
+    item->setCategory(nullptr);
+    EXPECT_EQ(item->getCategory(), nullptr);
+}
+
+// Test specifici per ShoppingList
 class ShoppingListTest : public ::testing::Test {
 protected:
     void SetUp() override {
-        list = std::make_unique<ShoppingList>("Test List");
-        category = std::make_shared<Category>(CategoryType::FRUITS_VEGETABLES, "Frutta");
-        item1 = std::make_shared<Item>("Mele", 2, category);
-        item2 = std::make_shared<Item>("Banane", 3, category);
-        item3 = std::make_shared<Item>("Latte", 1, nullptr);
+        shoppingList = std::make_unique<ShoppingList>("Lista della spesa");
+        foodCategory = std::make_shared<FoodCategory>("Frutta", "Frutta fresca");
+        householdCategory = std::make_shared<HouseholdCategory>("Casa", "Prodotti per la casa");
+
+        item1 = std::make_shared<Item>("Mele", 3, foodCategory, "Bio");
+        item2 = std::make_shared<Item>("Pane", 1, foodCategory);
+        item3 = std::make_shared<Item>("Detersivo", 1, householdCategory);
     }
 
-    std::unique_ptr<ShoppingList> list;
-    std::shared_ptr<Category> category;
+    std::unique_ptr<ShoppingList> shoppingList;
+    std::shared_ptr<Category> foodCategory;
+    std::shared_ptr<Category> householdCategory;
     std::shared_ptr<Item> item1;
     std::shared_ptr<Item> item2;
     std::shared_ptr<Item> item3;
 };
 
 TEST_F(ShoppingListTest, Constructor) {
-    EXPECT_EQ(list->getName(), "Test List");
-    EXPECT_GT(list->getId(), 0);
-    EXPECT_EQ(list->getTotalItems(), 0);
-    EXPECT_EQ(list->getPurchasedItemsCount(), 0);
-    EXPECT_EQ(list->getRemainingItems(), 0);
+    EXPECT_EQ(shoppingList->getName(), "Lista della spesa");
+    EXPECT_GT(shoppingList->getId(), 0);
+    EXPECT_EQ(shoppingList->getTotalItems(), 0);
+    EXPECT_EQ(shoppingList->getPurchasedItemsCount(), 0);
+    EXPECT_EQ(shoppingList->getRemainingItems(), 0);
 }
 
 TEST_F(ShoppingListTest, AddItems) {
-    list->addItem(item1);
-    list->addItem(item2);
+    shoppingList->addItem(item1);
+    shoppingList->addItem(item2);
 
-    EXPECT_EQ(list->getTotalItems(), 2);
-    EXPECT_EQ(list->getRemainingItems(), 2);
-    EXPECT_EQ(list->getPurchasedItemsCount(), 0);
-
+    EXPECT_EQ(shoppingList->getTotalItems(), 2);
+    EXPECT_EQ(shoppingList->getRemainingItems(), 2);
+    EXPECT_EQ(shoppingList->getPurchasedItemsCount(), 0);
 }
 
-TEST_F(ShoppingListTest, RemoveItems) {
-    list->addItem(item1);
-    list->addItem(item2);
-
-    EXPECT_TRUE(list->removeItem(item1->getId()));
-    EXPECT_EQ(list->getTotalItems(), 1);
-    EXPECT_FALSE(list->removeItem(999)); // Non-existent ID
+TEST_F(ShoppingListTest, AddNullItem) {
+    int originalCount = shoppingList->getTotalItems();
+    shoppingList->addItem(nullptr);
+    EXPECT_EQ(shoppingList->getTotalItems(), originalCount);
 }
 
-TEST_F(ShoppingListTest, FindItems) {
-    list->addItem(item1);
-    list->addItem(item2);
+TEST_F(ShoppingListTest, FindItem) {
+    shoppingList->addItem(item1);
+    shoppingList->addItem(item2);
 
-    auto found = list->findItem(item1->getId());
+    auto found = shoppingList->findItem(item1->getId());
     EXPECT_EQ(found, item1);
 
-    auto foundByName = list->findItemByName("Banane");
-    EXPECT_EQ(foundByName, item2);
-
-    auto notFound = list->findItem(999);
+    auto notFound = shoppingList->findItem(9999);
     EXPECT_EQ(notFound, nullptr);
 }
 
-TEST_F(ShoppingListTest, MarkItemsPurchased) {
-    list->addItem(item1);
-    list->addItem(item2);
+TEST_F(ShoppingListTest, FindItemByName) {
+    shoppingList->addItem(item1);
+    shoppingList->addItem(item2);
 
-    list->markItemPurchased(item1->getId(), true);
+    auto found = shoppingList->findItemByName("Mele");
+    EXPECT_EQ(found, item1);
 
-    EXPECT_EQ(list->getPurchasedItemsCount(), 1);
-    EXPECT_EQ(list->getRemainingItems(), 1);
+    auto notFound = shoppingList->findItemByName("Inesistente");
+    EXPECT_EQ(notFound, nullptr);
+}
+
+TEST_F(ShoppingListTest, RemoveItem) {
+    shoppingList->addItem(item1);
+    shoppingList->addItem(item2);
+
+    EXPECT_TRUE(shoppingList->removeItem(item1->getId()));
+    EXPECT_EQ(shoppingList->getTotalItems(), 1);
+
+    EXPECT_FALSE(shoppingList->removeItem(9999));
+    EXPECT_EQ(shoppingList->getTotalItems(), 1);
+}
+
+TEST_F(ShoppingListTest, MarkItemPurchased) {
+    shoppingList->addItem(item1);
+    shoppingList->addItem(item2);
+
+    shoppingList->markItemPurchased(item1->getId(), true);
+
     EXPECT_TRUE(item1->isPurchased());
-    EXPECT_FALSE(item2->isPurchased());
+    EXPECT_EQ(shoppingList->getPurchasedItemsCount(), 1);
+    EXPECT_EQ(shoppingList->getRemainingItems(), 1);
+
+    shoppingList->markItemPurchased(item1->getId(), false);
+    EXPECT_FALSE(item1->isPurchased());
+    EXPECT_EQ(shoppingList->getPurchasedItemsCount(), 0);
 }
 
 TEST_F(ShoppingListTest, GetItemsByCategory) {
-    list->addItem(item1);
-    list->addItem(item2);
-    list->addItem(item3);
+    shoppingList->addItem(item1);
+    shoppingList->addItem(item2);
+    shoppingList->addItem(item3);
 
-    auto fruitsItems = list->getItemsByCategory(category);
-    EXPECT_EQ(fruitsItems.size(), 2);
+    auto foodItems = shoppingList->getItemsByCategory(foodCategory);
+    EXPECT_EQ(foodItems.size(), 2);
 
-    auto otherCategory = std::make_shared<Category>(CategoryType::DAIRY, "Latticini");
-    auto otherItems = list->getItemsByCategory(otherCategory);
-    EXPECT_EQ(otherItems.size(), 0);
+    auto householdItems = shoppingList->getItemsByCategory(householdCategory);
+    EXPECT_EQ(householdItems.size(), 1);
 }
 
 TEST_F(ShoppingListTest, GetPurchasedAndUnpurchasedItems) {
-    list->addItem(item1);
-    list->addItem(item2);
-    list->addItem(item3);
+    shoppingList->addItem(item1);
+    shoppingList->addItem(item2);
+    shoppingList->addItem(item3);
 
     item1->setPurchased(true);
     item2->setPurchased(false);
     item3->setPurchased(true);
 
-    auto purchased = list->getPurchasedItems();
-    auto unpurchased = list->getUnpurchasedItems();
+    auto purchased = shoppingList->getPurchasedItems();
+    auto unpurchased = shoppingList->getUnpurchasedItems();
 
     EXPECT_EQ(purchased.size(), 2);
     EXPECT_EQ(unpurchased.size(), 1);
     EXPECT_EQ(unpurchased[0], item2);
 }
 
-TEST_F(ShoppingListTest, ClearItems) {
-    list->addItem(item1);
-    list->addItem(item2);
+TEST_F(ShoppingListTest, ClearPurchasedItems) {
+    shoppingList->addItem(item1);
+    shoppingList->addItem(item2);
+    shoppingList->addItem(item3);
+
     item1->setPurchased(true);
+    item3->setPurchased(true);
 
-    list->clearPurchasedItems();
-    EXPECT_EQ(list->getTotalItems(), 1);
-    EXPECT_EQ(list->findItem(item2->getId()), item2);
+    shoppingList->clearPurchasedItems();
 
-    list->clearAllItems();
-    EXPECT_EQ(list->getTotalItems(), 0);
+    EXPECT_EQ(shoppingList->getTotalItems(), 1);
+    EXPECT_EQ(shoppingList->getItems()[0], item2);
+}
+
+TEST_F(ShoppingListTest, ClearAllItems) {
+    shoppingList->addItem(item1);
+    shoppingList->addItem(item2);
+    shoppingList->addItem(item3);
+
+    shoppingList->clearAllItems();
+
+    EXPECT_EQ(shoppingList->getTotalItems(), 0);
+    EXPECT_TRUE(shoppingList->getItems().empty());
 }
 
 TEST_F(ShoppingListTest, SetName) {
-    list->setName("New Name");
-    EXPECT_EQ(list->getName(), "New Name");
+    shoppingList->setName("Nuova Lista");
+    EXPECT_EQ(shoppingList->getName(), "Nuova Lista");
+}
+
+TEST_F(ShoppingListTest, UniqueIds) {
+    ShoppingList list1("Lista 1");
+    ShoppingList list2("Lista 2");
+    EXPECT_NE(list1.getId(), list2.getId());
 }
 
 TEST_F(ShoppingListTest, EqualityOperators) {
-    ShoppingList list2("Another List");
-    EXPECT_NE(*list, list2);
-    EXPECT_TRUE(*list == *list);
-}
-
-TEST_F(ShoppingListTest, AddNullItem) {
-    list->addItem(nullptr);
-    EXPECT_EQ(list->getTotalItems(), 0);
+    ShoppingList list2("Altra Lista");
+    EXPECT_NE(*shoppingList, list2);
+    EXPECT_TRUE(*shoppingList == *shoppingList);
 }
--- tests/test_item.cpp
+++ tests/test_item.cpp
@@ -5,7 +5,8 @@
 class ItemTest : public ::testing::Test {
 protected:
     void SetUp() override {
-        category = std::make_shared<Category>(CategoryType::FRUITS_VEGETABLES, "Frutta");
+        // Usa FoodCategory invece di Category (che è astratta)
+        category = std::make_shared<FoodCategory>("Frutta e Verdura", "Frutta");
         item = std::make_unique<Item>("Mele", 3, category, "Bio");
     }
 
@@ -14,62 +15,62 @@ protected:
 };
 
 TEST_F(ItemTest, Constructor) {
-EXPECT_EQ(item->getName(), "Mele");
-EXPECT_EQ(item->getQuantity(), 3);
-EXPECT_FALSE(item->isPurchased());
-EXPECT_EQ(item->getCategory(), category);
-EXPECT_EQ(item->getNotes(), "Bio");
-EXPECT_GT(item->getId(), 0);
+    EXPECT_EQ(item->getName(), "Mele");
+    EXPECT_EQ(item->getQuantity(), 3);
+    EXPECT_FALSE(item->isPurchased());
+    EXPECT_EQ(item->getCategory(), category);
+    EXPECT_EQ(item->getNotes(), "Bio");
+    EXPECT_GT(item->getId(), 0);
 }
 
 TEST_F(ItemTest, DefaultConstructor) {
-Item defaultItem("Pane");
-EXPECT_EQ(defaultItem.getName(), "Pane");
-EXPECT_EQ(defaultItem.getQuantity(), 1);
-EXPECT_FALSE(defaultItem.isPurchased());
-EXPECT_EQ(defaultItem.getCategory(), nullptr);
-EXPECT_EQ(defaultItem.getNotes(), "");
+    Item defaultItem("Pane");
+    EXPECT_EQ(defaultItem.getName(), "Pane");
+    EXPECT_EQ(defaultItem.getQuantity(), 1);
+    EXPECT_FALSE(defaultItem.isPurchased());
+    EXPECT_EQ(defaultItem.getCategory(), nullptr);
+    EXPECT_EQ(defaultItem.getNotes(), "");
 }
 
 TEST_F(ItemTest, SettersAndGetters) {
-item->setName("Pere");
-item->setQuantity(5);
-item->setPurchased(true);
-item->setNotes("Mature");
+    item->setName("Pere");
+    item->setQuantity(5);
+    item->setPurchased(true);
+    item->setNotes("Mature");
 
-EXPECT_EQ(item->getName(), "Pere");
-EXPECT_EQ(item->getQuantity(), 5);
-EXPECT_TRUE(item->isPurchased());
-EXPECT_EQ(item->getNotes(), "Mature");
+    EXPECT_EQ(item->getName(), "Pere");
+    EXPECT_EQ(item->getQuantity(), 5);
+    EXPECT_TRUE(item->isPurchased());
+    EXPECT_EQ(item->getNotes(), "Mature");
 }
 
 TEST_F(ItemTest, InvalidQuantity) {
-int originalQuantity = item->getQuantity();
-item->setQuantity(-1);
-EXPECT_EQ(item->getQuantity(), originalQuantity);
+    int originalQuantity = item->getQuantity();
+    item->setQuantity(-1);
+    EXPECT_EQ(item->getQuantity(), originalQuantity);
 
-item->setQuantity(0);
-EXPECT_EQ(item->getQuantity(), originalQuantity);
+    item->setQuantity(0);
+    EXPECT_EQ(item->getQuantity(), originalQuantity);
 }
 
 TEST_F(ItemTest, UniqueIds) {
-Item item1("Item1");
-Item item2("Item2");
-EXPECT_NE(item1.getId(), item2.getId());
+    Item item1("Item1");
+    Item item2("Item2");
+    EXPECT_NE(item1.getId(), item2.getId());
 }
 
 TEST_F(ItemTest, EqualityOperators) {
-Item item2("Diverse", 1, nullptr, "");
-EXPECT_NE(*item, item2);
-EXPECT_TRUE(*item == *item);
+    Item item2("Diverse", 1, nullptr, "");
+    EXPECT_NE(*item, item2);
+    EXPECT_TRUE(*item == *item);
 }
 
 TEST_F(ItemTest, CategoryAssignment) {
-auto newCategory = std::make_shared<Category>(CategoryType::DAIRY, "Latticini");
-item->setCategory(newCategory);
-EXPECT_EQ(item->getCategory(), newCategory);
+    // Usa HouseholdCategory invece di Category con enum
+    auto newCategory = std::make_shared<HouseholdCategory>("Latticini", "Prodotti caseari");
+    item->setCategory(newCategory);
+    EXPECT_EQ(item->getCategory(), newCategory);
 
-item->setCategory(nullptr);
-EXPECT_EQ(item->getCategory(), nullptr);
+    item->setCategory(nullptr);
+    EXPECT_EQ(item->getCategory(), nullptr);
 }
-
